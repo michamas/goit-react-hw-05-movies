85a84fe8ce850b4bd5077f70f07e128a491ea164
Michał Masiulanis <m.masiulanis@gmail.com>
2023-05-01 16:03:59 +0200

    useEffect fix

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -30,7 +30,7 @@ const MovieDetails = () => {
 
   useEffect(() => {
     fetchDetails();
-  }, []);
+  });
 
   return (
     <>
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -25,7 +25,7 @@ const Reviews = () => {
   };
   useEffect(() => {
     fetchReviews();
-  }, []);
+  });
 
   return (
     <>
